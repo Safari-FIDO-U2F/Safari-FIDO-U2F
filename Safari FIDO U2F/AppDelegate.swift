9525a21b303a80d299e0eb31c953e30cf9dadfd2
BlahGeek <i@BlahGeek.com>
2016-10-13 17:00:20 +0800

    Initial Commit

---
+++ Safari FIDO U2F/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Safari FIDO U2F
+//
+//  Created by Yikai Zhao on 10/13/16.
+//  Copyright © 2016 Yikai Zhao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Safari FIDO U2F/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Safari FIDO U2F
+//
+//  Created by Yikai Zhao on 10/13/16.
+//  Copyright © 2016 Yikai Zhao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
