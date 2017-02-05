16c04b54270614af78f6e27c5aeb3f03127c11bf
Sam Deane <sam@elegantchaos.com>
2017-02-05 14:40:51 +0000

    Refactored request parsing and differences between requests into a standalone class.

--- Safari FIDO U2F Extension/SafariExtensionHandler.swift
+++ Safari FIDO U2F Extension/SafariExtensionHandler.swift
@@ -17,6 +17,81 @@ enum U2FError: Error {
 let U2F_V2 = "U2F_V2"
 let U2F_NODEVICE_RETRY_COUNT = 10
 
+class U2FRequest {
+    let appId : String
+    let challenge : String
+    let origin : String
+
+    init?(info : [String : Any], origin : String) {
+        self.origin = origin
+        if let appId = info["appId"] as? String, let challenge = info["challenge"] as? String {
+            self.appId = appId
+            self.challenge = challenge
+        } else {
+            return nil
+        }
+    }
+
+    static func ParseRequest(name : String, info : [String : Any]?, properties : SFSafariPageProperties?) throws -> U2FRequest {
+        var request : U2FRequest?
+
+        if let scheme = properties?.url?.scheme, let host = properties?.url?.host {
+            let origin = scheme + "://" + host
+            if let info = info {
+                switch name {
+                case "U2FSign":
+                    request = U2FSignRequest(info:info, origin:origin)
+
+                case "U2FRegister":
+                    request = U2FRegisterRequest(info:info, origin:origin)
+                }
+            }
+        } else {
+            throw U2FError.unknown(in: "bad origin")
+        }
+
+        guard request != nil else {
+            throw U2FError.badrequest()
+        }
+
+        return request!
+    }
+
+    func ChallengeDictionary() -> [String:String] {
+        return ["challenge": self.challenge, "version": U2F_V2, "appId": self.appId]
+    }
+
+    func Challenge() -> String {
+        let dict = self.ChallengeDictionary()
+        let bytes = try JSONSerialization.data(withJSONObject:dict)
+        let challenge = String.init(data:bytes, encoding: .utf8)!
+        return
+    }
+}
+
+class U2FRegisterRequest : U2FRequest {
+}
+
+class U2FSignRequest : U2FRequest {
+    let keyHandle : String
+
+    override init?(info : [String : Any], origin: String) {
+        if let keyHandle = info["keyHandle"] as? String {
+            self.keyHandle = keyHandle
+        } else {
+            return nil
+        }
+    }
+
+    override func ChallengeDictionary() -> [String:String] {
+        var dict = super.ChallengeDictionary()
+        dict["keyHandle"] = self.keyHandle
+        return dict
+    }
+
+}
+
+
 class SafariExtensionHandler: SFSafariExtensionHandler {
     
     func _sendResponse(page: SFSafariPage, error: U2FError?, result: String?) {
@@ -35,33 +110,10 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         if let result = result {
             userinfo["result"] = result
         }
-        page.dispatchMessageToScript(withName: "response", userInfo: userinfo)
+        page.dispatchMessageToScript(withName: "U2FResponse", userInfo: userinfo)
     }
     
-    override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
-        // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
-        page.getPropertiesWithCompletionHandler { properties in
-            
-            // userInfo must contains following keys:
-            //   appId: string
-            //   challenge: string
-            // if messageName == "sign":
-            //   keyHandle: string
-            
-            let appId = userInfo?["appId"] as? String
-            let challenge = userInfo?["challenge"] as? String
-            let keyHandle = userInfo?["keyHandle"] as? String
-            
-            guard appId != nil && challenge != nil && (messageName != "sign" || keyHandle != nil) else {
-                self._sendResponse(page: page, error: U2FError.badrequest(), result: nil)
-                return
-            }
-            
-            guard properties?.url?.scheme != nil && properties?.url?.host != nil else {
-                self._sendResponse(page: page, error: U2FError.unknown(in: "get_origin"), result: nil)
-                return
-            }
-            let origin = properties!.url!.scheme! + "://" + properties!.url!.host!
+    func processRequest(request : U2FRequest, from page: SFSafariPage) {
             
             var ret: u2fh_rc
             var devs: OpaquePointer?
@@ -91,11 +143,11 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
                 }
                 
                 var response: UnsafeMutablePointer<Int8>? = nil
-                if messageName == "register" {
+            if messageName == "U2FRegister" {
                     let chal_bytes = try JSONSerialization.data(withJSONObject: ["challenge": challenge!, "version": U2F_V2, "appId": appId!])
                     let chal = String.init(data: chal_bytes, encoding: .utf8)!
                     ret = u2fh_register(devs!, chal, origin, &response, U2FH_REQUEST_USER_PRESENCE)
-                } else if messageName == "sign" {
+            } else if messageName == "U2FSign" {
                     let chal_bytes = try JSONSerialization.data(withJSONObject: ["challenge": challenge!, "version": U2F_V2, "appId": appId!, "keyHandle": keyHandle!])
                     let chal = String.init(data: chal_bytes, encoding: .utf8)!
                     ret = u2fh_authenticate(devs!, chal, origin, &response, U2FH_REQUEST_USER_PRESENCE)
@@ -121,6 +173,25 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
                 u2fh_devs_done(devs)
             }
             u2fh_global_done()
+    }
+
+    override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
+        // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
+
+        page.getPropertiesWithCompletionHandler { properties in
+            do {
+
+                let request = U2FSignRequest.ParseRequest(name: messageName, info:userInfo, properties:properties)
+                processRequest(request: request, from:page)
+
+            } catch let error as U2FError {
+
+                self._sendResponse(page: page, error: error, result: nil)
+
+            } catch {
+
+                self._sendResponse(page: page, error: U2FError.unknown(in: "unknown"), result: nil)
+            }
         }
     }
 
