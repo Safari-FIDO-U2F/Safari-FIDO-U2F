89a818db49f80fde8e0e24eef54d3d846adacfe4
Sam Deane <sam@elegantchaos.com>
2018-01-10 12:16:17 +0000

    Fixed indent.

--- Safari FIDO U2F/ViewController.swift
+++ Safari FIDO U2F/ViewController.swift
@@ -18,53 +18,53 @@ let EXT_ID = "com.safari-fido-u2f.app.extension"
 let HOMEPAGE = "https://github.com/Safari-FIDO-U2F/Safari-FIDO-U2F"
 
 class ViewController: NSViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    let link = NSMutableAttributedString.init(string: HOMEPAGE)
-    let range = NSRange.init(location: 0, length: link.length)
-    link.addAttribute(NSAttributedStringKey.link, value: HOMEPAGE, range: range)
-    let paragraphStyle = NSMutableParagraphStyle()
-    paragraphStyle.alignment = NSTextAlignment.center
-    link.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: range)
-    self.hyperlinkField.attributedStringValue = link;
-    self.updateExtensionStatus(self.extensionUpdateBtn)
-    // Do any additional setup after loading the view.
-  }
-  
-  override var representedObject: Any? {
-    didSet {
-      // Update the view, if already loaded.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let link = NSMutableAttributedString.init(string: HOMEPAGE)
+        let range = NSRange.init(location: 0, length: link.length)
+        link.addAttribute(NSAttributedStringKey.link, value: HOMEPAGE, range: range)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.center
+        link.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: range)
+        self.hyperlinkField.attributedStringValue = link;
+        self.updateExtensionStatus(self.extensionUpdateBtn)
+        // Do any additional setup after loading the view.
     }
-  }
-  
-  
-  @IBOutlet weak var hyperlinkField: NSTextField!
-  @IBOutlet weak var extensionUpdateBtn: NSButton!
-  @IBOutlet weak var extensionStatusLabel: NSTextField!
-  
-  @IBAction func updateExtensionStatus(_ sender: NSButton) {
-    SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: EXT_ID) { (state, error) in
-      if error != nil {
-        print("Error determining the state of extension: \(error!)");
-        return;
-      }
-      
-      DispatchQueue.main.async {
-        let label = state!.isEnabled ? "Enabled" : "Disabled"
-        print("Extension: \(label)")
-        self.extensionStatusLabel.stringValue = label
-      }
-      
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
     }
-  }
-  
-  @IBAction func enableSafariExtension(_ sender: NSButton) {
-    SFSafariApplication.showPreferencesForExtension(withIdentifier: EXT_ID) { (_) in
-      self.updateExtensionStatus(self.extensionUpdateBtn)
+    
+    
+    @IBOutlet weak var hyperlinkField: NSTextField!
+    @IBOutlet weak var extensionUpdateBtn: NSButton!
+    @IBOutlet weak var extensionStatusLabel: NSTextField!
+    
+    @IBAction func updateExtensionStatus(_ sender: NSButton) {
+        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: EXT_ID) { (state, error) in
+            if error != nil {
+                print("Error determining the state of extension: \(error!)");
+                return;
+            }
+            
+            DispatchQueue.main.async {
+                let label = state!.isEnabled ? "Enabled" : "Disabled"
+                print("Extension: \(label)")
+                self.extensionStatusLabel.stringValue = label
+            }
+            
+        }
     }
-  }
-  
+    
+    @IBAction func enableSafariExtension(_ sender: NSButton) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: EXT_ID) { (_) in
+            self.updateExtensionStatus(self.extensionUpdateBtn)
+        }
+    }
+    
 }
 
