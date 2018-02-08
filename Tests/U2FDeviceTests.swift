b911caa717b561213fe51211c5690ccc2f3b5c23
Sam Deane <sam@elegantchaos.com>
2018-02-08 22:45:20 +0000

    added signing tests

--- Tests/U2FDeviceTests.swift
+++ Tests/U2FDeviceTests.swift
@@ -9,9 +9,10 @@
 
 import XCTest
 
+let testAppIdQuoted = "https:\\/\\/demo.yubico.com"
 let testRegistrationData = "BQR_5ng1-yI6hc6x20RVjMYuHJTyp6biyrzl5yT0V4-0d4ywEQgGAXMUcnI07l1Gv2Kf9qgnK1zlHFz6Y0zjGxq5UPo27WMf_SCHTQHMQN-jN3WArnk47oOrmy7NYl41QbVeGPXLCGYzHs_WnlWoRbt7DyZTVDyfsEsfH0zeFbm3Y6Sl3VAhnDjd2f2zSsXhufmNMIIBVTCB_aADAgECAgqWcVVHVVeAljgVMAoGCCqGSM49BAMCMBcxFTATBgNVBAMTDEZUIEZJRE8gMDEwMDAeFw0xNDA4MTQxODI5MzJaFw0yNDA4MTQxODI5MzJaMDExLzAtBgNVBAMTJlUyRiBTZWN1cml0eSBLZXktLTk2NzE1NTQ3NTU1NzgwOTYzODE1MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE89DW0e8xFP_goJ2H3EVKu94kukiBXs0qww2jXZLAdsafSEPFRHoFP0JLhWykVgpEjnJ3dm_vqsFih4wPg1pj7qMXMBUwEwYLKwYBBAGC5RwCAQEEBAMCBSAwCgYIKoZIzj0EAwIDRwAwRAIgUKY-UEhLxxArC4Mj7ZzpIUvix7aY33TGCh6ZhAvhgPQCIARqXiMcOpGWdao7qfBju7D9LJqljYTSACYRvD1a8WmFMEQCIE7CDjf7Ap-Mzx2FQyIQeqNETiTtXmICFWCHE8ITr6rRAiBKMaOugkOOIakLD-9GLQIC-jPVfqC4pmDcvK4xf1XPZw"
 let testClientData = "eyAiY2hhbGxlbmdlIjogIkVlZlJrWGc2UTZIaEdwVTI4U1NCYmpVX0FsNmV6VDV6V1dvNmd3R0prQVkiLCAib3JpZ2luIjogImh0dHA6XC9cL3Rlc3Qub3JpZ2luIiwgInR5cCI6ICJuYXZpZ2F0b3IuaWQuZmluaXNoRW5yb2xsbWVudCIgfQ"
-let testRegisterChallenge = "{\"challenge\":\"\(testChallenge)\",\"version\":\"U2F_V2\",\"appId\":\"\(testAppId)\"}"
+let testRegisterChallenge = "{\"challenge\":\"\(testChallenge)\",\"version\":\"U2F_V2\",\"appId\":\"\(testAppIdQuoted)\"}"
 let testRegisterResponse = "{ \"registrationData\": \"\(testRegistrationData)\", \"clientData\": \"\(testClientData)\" }"
 
 
@@ -20,7 +21,7 @@ let testRegisterResponse = "{ \"registrationData\": \"\(testRegistrationData)\",
 */
 
 let testSignatureData = "AQAAABcwRgIhAPpHzFHIVZRGlSJvOJ452d3Cbxd00pObjwKZVbWfHt7PAiEArliUes968d98oTENLS-BC1Vkwrxw59yb5Q0KLTtM6hE"
-let testSignChallenge = "{ \"version\":\"U2F_V2\",\"challenge\":\"\(testChallenge)\",\"keyHandle\":\"\(testKeyHandle)\",\"appId\":\"\(testAppId)\"}"
+let testSignChallenge = "{\"version\":\"U2F_V2\",\"challenge\":\"\(testChallenge)\",\"keyHandle\":\"\(testKeyHandle)\",\"appId\":\"\(testAppIdQuoted)\"}"
 let testSignResponse = "{ \"signatureData\": \"\(testSignatureData)\", \"clientData\": \"\(testClientData)\", \"keyHandle\": \"\(testKeyHandle)\" }"
 
 class MockDevice : U2FDevice {
@@ -46,7 +47,22 @@ class MockDevice : U2FDevice {
 
 class U2FDeviceTests: XCTestCase {
 
-    func testRegister() {
+    func testRegistrationChallenge() {
+        guard let request = U2FRegisterRequest(requestDictionary: testRegisterRequest, origin: testOrigin) else {
+            XCTFail()
+            return
+        }
+        
+        do {
+            let device = try MockDevice(response: testRegisterResponse)
+            let _ = try device.perform(request: request)
+            XCTAssertEqual(device.request, testRegisterChallenge)
+        } catch {
+            XCTFail()
+        }
+    }
+
+    func testRegistrationResponseData() {
         guard let request = U2FRegisterRequest(requestDictionary: testRegisterRequest, origin: testOrigin) else {
             XCTFail()
             return
@@ -55,9 +71,6 @@ class U2FDeviceTests: XCTestCase {
         do {
             let device = try MockDevice(response: testRegisterResponse)
             let response = try device.perform(request: request)
-            
-            print(device.request)
-            
             XCTAssertEqual(response.type, U2FRegisterRequest.ResponseType)
             XCTAssertEqual(response.requestId, request.requestId)
             
@@ -81,7 +94,24 @@ class U2FDeviceTests: XCTestCase {
         
     }
 
-    func testSign() {
+    func testSigningChallenge() {
+        guard let request = U2FSignRequest(requestDictionary: testSignRequest, origin: testOrigin) else {
+            XCTFail()
+            return
+        }
+        
+        do {
+            let device = try MockDevice(response: testSignResponse)
+            let _ = try device.perform(request: request)
+
+            XCTAssertEqual(device.request, testSignChallenge)
+        } catch {
+            XCTFail()
+        }
+        
+    }
+
+    func testSigningResponse() {
         guard let request = U2FSignRequest(requestDictionary: testSignRequest, origin: testOrigin) else {
             XCTFail()
             return
@@ -90,9 +120,6 @@ class U2FDeviceTests: XCTestCase {
         do {
             let device = try MockDevice(response: testSignResponse)
             let response = try device.perform(request: request)
-
-            print(device.request)
-
             XCTAssertEqual(response.type, U2FSignRequest.ResponseType)
             XCTAssertEqual(response.requestId, request.requestId)
             
